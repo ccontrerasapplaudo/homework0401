b79f8d525f5827365f38230ed8487c4fa4d27a7b
Carlos Rafael Contreras <103429472+carloscontreras0297@users.noreply.github.com>
2022-08-28 21:43:45 -0600

    Creating Exercise1 Class with methods "browserStart": open the browser. And method "codePause": to pause code execution and pause it 2 seconds.

--- src/test/java/Exercise1.java
+++ src/test/java/Exercise1.java
@@ -1,6 +1,48 @@
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import java.lang.Thread;
+
 public class Exercise1 {
 
-    public void verifyPageTitle(){
+    private WebDriver driver;
+    private String browserName;
 
+    public Exercise1(WebDriver driver, String browserName) {
+        this.driver = driver;
+        this.browserName = browserName;
+        driver.manage().window().maximize();
     }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getBrowserName() {
+        return browserName;
+    }
+
+    public void setBrowserName(String browserName) {
+        this.browserName = browserName;
+    }
+
+    public void browserStart(String webPage){
+        System.out.println(browserName+": Navigating to page "+webPage);
+        driver.get(webPage);
+        codePause();
+    }
+
+    public void codePause(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
--- src/test/java/MainClass.java
+++ src/test/java/MainClass.java
@@ -7,41 +7,24 @@ public class MainClass {
     public static void main(String[] args) {
 
         // Setting up driver locations
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\carlo\\Desktop\\Selenium Jar and Drivers\\chromedriver.exe");
-        System.setProperty("webdriver.gecko.driver","C:\\Users\\carlo\\Desktop\\Selenium Jar and Drivers\\geckodriver.exe");
-        System.setProperty("webdriver.edge.driver","C:\\Users\\carlo\\Desktop\\Selenium Jar and Drivers\\msedgedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\carlo\\Desktop\\Selenium Jar and Drivers\\chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\carlo\\Desktop\\Selenium Jar and Drivers\\geckodriver.exe");
+        System.setProperty("webdriver.edge.driver", "C:\\Users\\carlo\\Desktop\\Selenium Jar and Drivers\\msedgedriver.exe");
 
-        // Testing code with firefox browser
+
+        //******************************* Exercise 1 *******************************
+
+        System.out.println("Executing exercise 1: \n");
         // Create firefox webdriver
         WebDriver firefoxDriver = new FirefoxDriver();
-        // Maximize windows
-        firefoxDriver.manage().window().maximize();
-        // Navigate to google.com
-        firefoxDriver.get("https://google.com");
-        // Get webpage title
-        System.out.println(firefoxDriver.getTitle());
-        // Get webpage URL
-        System.out.println(firefoxDriver.getCurrentUrl());
-        // Navigate to yahoo.com
-        firefoxDriver.navigate().to("http://yahoo.com");
-        // Navigate back to google.com
-        firefoxDriver.navigate().back();
-        //Navigate back to yahoo.com
-        firefoxDriver.navigate().forward();
+        //Create exercise1 object and sending firefoxDriver
+        Exercise1 exercise1 = new Exercise1(firefoxDriver,"Firefox");
+        //Starts the Firefox browser and navigates to http://opencart.abstracta.us/index.php?route=common/home
+        exercise1.browserStart("http://opencart.abstracta.us/index.php?route=common/home");
+        System.out.println("\nExercise 1 Completed!\n");
 
-        // Close all firefox instances
-        firefoxDriver.quit();
 
-        // Perform testing with chrome webdriver code
-        WebDriver chromeDriver = new ChromeDriver();
-        chromeDriver.manage().window().maximize();
-        chromeDriver.get("https://google.com");
-        chromeDriver.quit();
+        //******************************* Exercise 2 *******************************
 
-        // Perform testing with edge webdriver code
-        WebDriver edgeDriver = new EdgeDriver();
-        edgeDriver.manage().window().maximize();
-        edgeDriver.get("https://google.com");
-        edgeDriver.close();
     }
 }
